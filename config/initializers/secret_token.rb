14a5ee2821105d2655ec6bc4dde2775d8d23443f
vbrao <bharadwajracherla@gmail.com>
2012-12-16 18:06:56 +0000

    add newsfeed and facebook api..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acs::Application.config.secret_token = 'b56a0911114359ff44dafe166fb448e0d96f9b78d3a119ef8d8d6ed812ead347dc1a8cb18674479f774b2e7ed9a021725c38fae3229691bc94d2bdd0d35eccbe'
